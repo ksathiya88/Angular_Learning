fb674e484af72a1fc63cb215ae09fe6dd7842873
Karthik S <37337298+ksathiya88@users.noreply.github.com>
2019-07-17 15:56:50 +0530

    Adding libraries

--- e2e/actions.ts
+++ e2e/actions.ts
@@ -1,13 +1,14 @@
-import { promise as wdpromise } from "selenium-webdriver";
-import { element, by } from "protractor";
+import {promise as wdpromise} from 'selenium-webdriver';
+import {element, by} from 'protractor';
+
 
 export function setElement(id: string, value: string): wdpromise.Promise<any> {
   const elemObj = element(by.id(id));
   return elemObj.getTagName().then((tagName: string) => {
-    return elemObj.getAttribute("type").then((attributeType: string) => {
-      if (tagName === "select") {
+    return elemObj.getAttribute('type').then((attributeType: string) => {
+      if (tagName === 'select') {
         return selectProperty(id, value);
-      } else if (attributeType === "radio" || attributeType === "checkbox") {
+      } else if (attributeType === 'radio' || attributeType === 'checkbox') {
         return setCheckOrRadio(id, value);
       } else {
         return setInput(id, value);
@@ -22,7 +23,7 @@ function selectProperty(id: string, value: string) {
 
 function setCheckOrRadio(id: string, value: string) {
   const elemObj = element(by.id(id));
-  const checked = value === "true";
+  const checked = value === 'true';
   elemObj.isSelected().then(selected => {
     if (selected !== checked) {
       return clickCheckOrRadio(id);
@@ -32,7 +33,7 @@ function setCheckOrRadio(id: string, value: string) {
 
 function clickCheckOrRadio(id: string) {
   return element(by.id(id))
-    .element(by.xpath(".."))
+    .element(by.xpath('..'))
     .click();
 }
 
@@ -46,3 +47,42 @@ function setInput(id: string, value: string) {
 export function elemClick(id: string) {
   return element(by.id(id)).click();
 }
+
+export interface IElement {
+  id: string;
+  value: string;
+}
+
+export function elementsSeq(arrayElements: IElement[]) {
+  return arrayElements.reduce((promise, item) => {
+    promise.then(() => {
+      return setElement(item.id, item.value);
+    });
+  }, protractor.promise.fulfilled());
+}
+
+export function elementsRandom(arrayElements: IElement[]) {
+  const deferredAll = arrayElements.map((item) => {
+    setElement(item.id, item.value);
+
+  });
+  return protractor.promise.all(deferredAll);
+}
+
+export function setValues(ids: {}, values: {}) {
+  return elementsSeq((getElements(ids, values)));
+}
+
+export function getElements(ids: {}, values: {}) {
+  let elements: IElement[] = [];
+  for (let key in object.keys(ids)) {
+    if (key in values) {
+      let value: string = values[key];
+      let id: string = ids[key];
+      elements.push({id: id, value: value});
+    }
+  }
+  return elements;
+}
+
+
